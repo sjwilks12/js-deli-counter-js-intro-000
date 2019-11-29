6f864ac3f017f82c2272dcd9a6c08d92b53f0097
Samuel J Wilkins <wilkins.samuelj@gmail.com>
2019-11-29 05:44:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function takeANumber (currentLine, newPerson) {
   var spot = currentLine.length + 1
+  currentLine.push(newPerson)
   return `Welcome, ${newPerson}. You are number ` + spot + " in line."
 }
+
