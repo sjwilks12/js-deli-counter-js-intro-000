e66000611ab959441544c395b059724154466c67
Samuel J Wilkins <wilkins.samuelj@gmail.com>
2019-11-29 05:58:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ function takeANumber (currentLine, newPerson) {
   currentLine.push(newPerson)
   return `Welcome, ${newPerson}. You are number ` + spot + " in line."
 }
+
+function nowServing (currentLine) {
+  if (currentLine.length > 0) {
+    var upNow = currentLine[0]
+    currentLine.shift()
+    return upNow
+  }
+  else {
+    return "There is nobody waiting to be served!"
+  }
+}
